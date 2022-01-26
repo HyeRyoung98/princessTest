a9ca4a61647766289b4eca9e8d3c71fac03c509b
khr <sykhrb793005@naver.com>
2022-01-27 00:40:22 +0900

    add QnA.class

---
+++ app/src/main/java/com/example/princesstest/QnA.java
@@ -0,0 +1,15 @@
+package com.example.princesstest;
+
+public class QnA {
+    private QnA(){}
+
+    public String qes[] = {"Q1. 선상파티에 초대받았어! 무슨색 드레스를 입고 가지?",
+            "Q2. 선상파티에 초대받았어! 무슨색 신발을 신고 가지?",
+            "Q3. 선상파티에 초대받았어! 무슨색 가방을 들고 가지?"};
+
+    public int img[] = {R.drawable.dress, R.drawable.shoes, R.drawable.bag};
+
+    public String ans[][] = { {"분홍색", "노란색", "하늘색", "빨간색"}, {"파란색", "초록색", "회색"},
+            {"검정색", "흰색", "보라색"}};
+
+}
